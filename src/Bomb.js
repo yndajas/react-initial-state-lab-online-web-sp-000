42e60758d636c3afce7dee90b798c5b528ec9156
Ynda Jas <yndajas@gmail.com>
2021-04-20 04:11:57 +0100

    commit tree a903bee3c5b66aad02bd7180aabcefd79c85e99b

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,17 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+    constructor(props) {
+        super()
+        this.state = {secondsLeft: props.initialCount}
+    }
+
+    render() {
+        if (this.state.secondsLeft === 0) {
+            return 'Boom!';
+        } else {
+            return `${this.state.secondsLeft} seconds left before I go boom!`;
+        }
+    }
+}
